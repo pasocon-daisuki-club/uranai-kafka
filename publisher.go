2c36819ce928d988595761cd2e6c125efa154ca6
Hiroki Mizukami <hiroki@Hirokis-MacBook-Pro.local>
2024-06-25 04:48:51 +0900

    init

--- publisher.go
+++ publisher.go
@@ -2,13 +2,32 @@ package uranai
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
 type Publisher struct {
-	c *kafka.Producer
+	c         *kafka.Producer
+	partition *kafka.TopicPartition
 }
 
-func (c *Publisher) Publish(ctx context.Context, result *ResultSet) error {
+func (p *Publisher) Publish(ctx context.Context, resultSet *ResultSet) error {
+
+	for _, result := range resultSet.Results {
+		// Convert result to JSON
+		jsonBytes, err := json.Marshal(result)
+		if err != nil {
+			return err
+		}
+		// Publish to Kafka
+		message := &kafka.Message{
+			TopicPartition: *p.partition,
+			Value:          jsonBytes,
+		}
+		err = p.c.Produce(message, nil)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
