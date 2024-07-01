69a0b11cfecb95fa0e9a9c81f35f4fdde4f7b4c6
Hiroki Mizukami <hiroki@Hirokis-MacBook-Pro.local>
2024-07-01 17:38:53 +0900

    impl: sarama publisher

--- publisher.go
+++ publisher.go
@@ -3,7 +3,9 @@ package uranai
 import (
 	"context"
 	"encoding/json"
+	"github.com/IBM/sarama"
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"log"
 )
 
 type Publisher interface {
@@ -38,5 +40,31 @@ func (p *ConfluentPublisher) Publish(ctx context.Context, resultSet *ResultSet) 
 		}
 	}
 	p.c.Flush(15 * 1000)
+	return nil
+}
+
+type SaramaPublisher struct {
+	c sarama.SyncProducer
+}
+
+func (s SaramaPublisher) Publish(ctx context.Context, resultSet *ResultSet) error {
+	for _, result := range resultSet.Results {
+		// Convert result to JSON
+		jsonBytes, err := json.Marshal(result)
+		if err != nil {
+			return err
+		}
+		// Publish to Kafka
+		message := &sarama.ProducerMessage{
+			Topic: "uranai-kafka", //FIXME: Hardcoded topic
+			Value: sarama.StringEncoder(jsonBytes),
+		}
+		partition, offset, err := s.c.SendMessage(message)
+		if err != nil {
+			return err
+		} else {
+			log.Printf("> message sent to partition %d at offset %d\n", partition, offset)
+		}
+	}
 	return nil
 }
--- publisher_test.go
+++ publisher_test.go
@@ -65,7 +65,7 @@ func TestPublisher_Publish(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			p := &Publisher{
+			p := &ConfluentPublisher{
 				c:         tt.fields.c,
 				partition: tt.fields.partition,
 			}
